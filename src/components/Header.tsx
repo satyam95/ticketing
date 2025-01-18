9dded7c90c6814b6f3527a77f8529b5573c73267
Satyam Sagar <satyamsagar30@gmail.com>
2025-01-18 19:25:52 +0530

    kinde authentication implementation

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,9 @@
-import { File, HomeIcon, UsersRound } from "lucide-react";
+import { File, HomeIcon, LogOut, UsersRound } from "lucide-react";
 import { NavButton } from "./NavButton";
 import Link from "next/link";
 import { ModeToggle } from "./ModeToggle";
+import { Button } from "./ui/button";
+import { LogoutLink } from "@kinde-oss/kinde-auth-nextjs/components";
 
 export default function Header() {
   return (
@@ -23,6 +25,18 @@ export default function Header() {
           <NavButton href="/tickets" label="Tickets" icon={File} />
           <NavButton href="/customers" label="Customers" icon={UsersRound} />
           <ModeToggle />
+          <Button
+            variant="ghost"
+            size="icon"
+            aria-label="LogOut"
+            title="LogOut"
+            className="rounded-full"
+            asChild
+          >
+            <LogoutLink>
+              <LogOut />
+            </LogoutLink>
+          </Button>
         </div>
       </div>
     </header>
